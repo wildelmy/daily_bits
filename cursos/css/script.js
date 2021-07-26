d72766bc05e515baf2430a5711f4eae3042b5607
wildelmy <wildelmycolina@gmail.com>
2021-07-25 23:46:54 -0500

    agrega preguntas css y js

--- cursos/css/script.js
+++ cursos/css/script.js
@@ -12,24 +12,19 @@ const tipos_preguntas = {
 const preguntas = [
     {
         tipo: tipos_preguntas.seleccion,
-        texto: "¿Qué etiqueta es semánticamente correcta para el contenido principal?",
-        respuestas: ["main", "section", "header"],
+        texto: "¿Cuál es el valor predeterminado de la propiedad de posición",
+        respuestas: ["static", "fixed", "absolute", "relative"],
     },
     {
         tipo: tipos_preguntas.seleccion,
-        texto: "¿Qué etiqueta HTML nos sirve para incluir archivos de JavaScript?",
-        respuestas: ["&ltscript&gt", "&ltbr&gt", "&ltstyles&gt"]
+        texto: "¿Cómo selecciona todos los elementos p dentro de un elemento div?",
+        respuestas: ["div p", "div.p", "div+p"]
     },
     {
         tipo: tipos_preguntas.seleccion,
         texto: "¿Qué significa DRY?",
         respuestas: ["Don’t repeat yourself", "Don’t repeat yellow", "Don’t recicle year"]
     },
-    {
-        tipo: tipos_preguntas.ordenar,
-        texto: "Organiza la estructura de un documento HTML5",
-        opciones: ["&lt!DOCTYPE html&gt", "&lthtml&gt", "&lthead&gt &lthead&gt", "&ltbody&gt &lt/body&gt", "&lt/html&gt"]
-    }
 ];
 
 const juego = {
@@ -55,10 +50,6 @@ function jugar() {
 
     if (pregunta_aleatoria.tipo === tipos_preguntas.seleccion) {
         preguntaSeleccion()
-    }
-
-    if (pregunta_aleatoria.tipo === tipos_preguntas.ordenar) {
-        preguntaOrdenar()
     }
 
     preguntas.splice(indice_aleatorio, 1);
@@ -97,39 +88,10 @@ function comprobarPreguntaSeleccion() {
     }
 }
 
-function preguntaOrdenar() {
-    let html_opciones = "";
-    const orden_seleccionado = []
-    $('#pregunta').text(pregunta_aleatoria.texto)
-    let opciones = pregunta_aleatoria.opciones
-    for (let i in opciones) {
-        html_opciones += `
-            <button class="opcion-ordear mx-1 my-1" data-index="${i}">${opciones[i]}</button>
-        `
-    }
-    $('#seccion-ordenar').html(html_opciones)
-    $('.opcion-ordear').click(function(ev) {
-        let indice_opcion = Number(ev.target.dataset.index);
-        $('#seccion-ordenada').append(`
-        <button class="opcion-ordear mx-1 my-1" data-index="${indice_opcion}">${opciones[indice_opcion]}</button>
-        `)
-        orden_seleccionado.push(opciones.splice(opciones[indice_opcion], 1));
-        let html_opciones = "";
-        for (let i in opciones) {
-            html_opciones += `
-                <button class="opcion-ordear mx-1 my-1" data-index="${i}">${opciones[i]}</button>
-            `
-        }
-        $('#seccion-ordenar').html(html_opciones)
-    });
-}
 
 function comprobar() {
     if (pregunta_aleatoria.tipo === tipos_preguntas.seleccion) {
         comprobarPreguntaSeleccion()
-    }
-    if (pregunta_aleatoria.tipo === tipos_preguntas.ordenar) {
-        preguntaSeleccion()
     }
     setTimeout(jugar, 1000)
 }
